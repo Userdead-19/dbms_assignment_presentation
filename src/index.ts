e7a0a48253accabc531d10f6edbf7dcc64c5f2c0
Abinav <abinav10102@gmail.com>
2024-05-19 16:34:43 +0530

    chore: Add endpoint for reading a file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import bodyParser = require("body-parser");
 import cors from "cors";
 import morgan from 'morgan';
+const fs = require('fs');
+const path = require('path');
 const ApiRouter = require('./Router/ApiRouter');
 dotenv.config();
 
@@ -30,6 +32,19 @@ app.use("/api", ApiRouter)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
+});
+
+app.get('/readfile', (req, res) => {
+    const filePath = path.join(__dirname, 'example.txt');
+
+    fs.readFile(filePath, 'utf8', (err: any, data: any) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            res.status(500).send('Error reading file');
+            return;
+        }
+        res.send(data);
+    });
 });
 
 app.listen(3000, () => {
